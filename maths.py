efa08f07a8db66208eb3f8c5da4c8c76f0f90d96
VishnuReddyAdena <vishnureddyadena7@gmail.com>
2025-06-25 09:41:40 +0530

    Add files via upload

---
+++ maths.py
@@ -0,0 +1,16 @@
+#two numbers input
+a = int(input("enter first number:"))
+b = int(input("enter second number:"))
+#mathematical operation
+#addition
+c = a+b
+#subtraction
+d = a-b
+#multiplication
+e = a*b
+#division
+f = a/b
+print("Addition :",c)
+print("Subtraction :",d)
+print("Multiplication :",e)
+print("Division :",f)
