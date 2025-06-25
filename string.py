b40ab6935b49326357c2a61f5ae596013357de3a
VishnuReddyAdena <vishnureddyadena7@gmail.com>
2025-06-25 09:49:28 +0530

    Add files via upload

--- string.py
+++ string.py
@@ -1,7 +1,4 @@
 #input names from user
 a =input("enter your first name:")
 b = input("enter your last name: ")
-#concatenation of first name and last name
-c = a + b
-#greeting
-print(f"Hello,{a +b}! Welcome to the python program")
+print("Hello",a+b,"! Welcome to the python program")
